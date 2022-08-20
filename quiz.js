f6a509a4519563387f8c4fb168f7564d83872e19
benNjogu <kamandemachafi@gmail.com>
2022-08-20 13:22:40 +0300

    adding questions

--- quiz.js
+++ quiz.js
@@ -10,3 +10,39 @@ const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const score = document.getElementById("score");
+
+//create questions
+let questions = [
+  {
+    question: "What does HTML stand for?",
+    imgSrc: "img/html.png",
+    choiceA: "correct",
+    choiceB: "wrong",
+    choiceC: "wrong",
+    correct: "A",
+  },
+  {
+    question: "What does CSS stand for?",
+    imgSrc: "img/css.png",
+    choiceA: "wrong",
+    choiceB: "correct",
+    choiceC: "wrong",
+    correct: "B",
+  },
+  {
+    question: "What does JS stand for?",
+    imgSrc: "img/js.png",
+    choiceA: "wrong",
+    choiceB: "wrong",
+    choiceC: "correct",
+    correct: "C",
+  },
+  {
+    question: "Bro do you even code?",
+    imgSrc: "img/brocon.png",
+    choiceA: "wrong",
+    choiceB: "wrong",
+    choiceC: "wrong",
+    correct: "none",
+  },
+];
